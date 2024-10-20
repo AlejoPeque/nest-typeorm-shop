9ace7c194bc2c8ad18005b9cfa4470ffb6514966
Alejo Pequeño <alejopequeno25@gmail.com>
2024-10-20 01:58:05 -0300

    feat: create, getAll, getById/Slug/Title and update Product (#1)

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Product {
@@ -8,6 +14,52 @@ export class Product {
   @Column('text', { unique: true })
   title: string;
 
-  @Column('numeric', { default: 0 })
+  @Column('float', { default: 0 })
   price: number;
+
+  @Column('text', { nullable: true })
+  description: string;
+
+  @Column('text', { unique: true })
+  slug: string;
+
+  @Column('int', { default: 0 })
+  stock: number;
+
+  @Column('text', { array: true })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+
+  @Column({
+    type: 'text',
+    array: true,
+    default: [],
+  })
+  tags: string[];
+
+  // images
+
+  // Antes de insertar
+  @BeforeInsert()
+  checkSlogInsert() {
+    if (!this.slug) {
+      this.slug = this.title;
+    }
+    this.slug = this.slug
+      .trim()
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
+
+  @BeforeUpdate()
+  checkSlogUpdate() {
+    this.slug = this.slug
+      .trim()
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
 }
